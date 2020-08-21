cf400cd726aaed2fc3a7dffef259aa1e93ca2b22
developer@whizgen.in <rileymanda1@gmail.com>
2020-08-21 05:47:39 +0530

    Updates

---
+++ precache-manifest.03eaf99ad1ee0dabefd72dcd98352ac6.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a67c724f208c3c36ae9f5e9c798b22",
+    "url": "/webshop/index.html"
+  },
+  {
+    "revision": "16ad1a189b0fc87a5058",
+    "url": "/webshop/static/css/1.3e1e16bd.chunk.css"
+  },
+  {
+    "revision": "ab01acd8529179255d44",
+    "url": "/webshop/static/css/10.a0060d0e.chunk.css"
+  },
+  {
+    "revision": "fd4c58a91c194f7f547a",
+    "url": "/webshop/static/css/6.11c35eea.chunk.css"
+  },
+  {
+    "revision": "10bcc873e92b17042026",
+    "url": "/webshop/static/css/7.eef2177b.chunk.css"
+  },
+  {
+    "revision": "9b53353ece9995597095",
+    "url": "/webshop/static/js/0.e649b1d2.chunk.js"
+  },
+  {
+    "revision": "16ad1a189b0fc87a5058",
+    "url": "/webshop/static/js/1.2eb8e9c0.chunk.js"
+  },
+  {
+    "revision": "ab01acd8529179255d44",
+    "url": "/webshop/static/js/10.b1c8e6b2.chunk.js"
+  },
+  {
+    "revision": "379b5f55e9324cd0d476",
+    "url": "/webshop/static/js/11.e5ba19fe.chunk.js"
+  },
+  {
+    "revision": "360a332f3387c37c1a2d",
+    "url": "/webshop/static/js/12.c58c6995.chunk.js"
+  },
+  {
+    "revision": "d2b519ddba142a25dc56",
+    "url": "/webshop/static/js/13.2fe9593f.chunk.js"
+  },
+  {
+    "revision": "01c4ac7e08890f565e19",
+    "url": "/webshop/static/js/14.8f969475.chunk.js"
+  },
+  {
+    "revision": "1d954d3bd494883dcade",
+    "url": "/webshop/static/js/15.9a715985.chunk.js"
+  },
+  {
+    "revision": "6adc590a0b904d062c3c",
+    "url": "/webshop/static/js/16.cbce14a8.chunk.js"
+  },
+  {
+    "revision": "f98356e1cb68acb46207",
+    "url": "/webshop/static/js/17.2e03cfbf.chunk.js"
+  },
+  {
+    "revision": "ec1990fc40dd50c3e88d",
+    "url": "/webshop/static/js/18.dd8a6d4a.chunk.js"
+  },
+  {
+    "revision": "a88dd49d5726a944f0db",
+    "url": "/webshop/static/js/19.92b3c1ff.chunk.js"
+  },
+  {
+    "revision": "33cbd7d62d968a3e51d7",
+    "url": "/webshop/static/js/2.3550b693.chunk.js"
+  },
+  {
+    "revision": "a10500978e7f6fe71f53",
+    "url": "/webshop/static/js/20.eac01e33.chunk.js"
+  },
+  {
+    "revision": "a2bf22089aab6766fe83",
+    "url": "/webshop/static/js/21.8900cd5c.chunk.js"
+  },
+  {
+    "revision": "db1efb0a4e5a20444f5e",
+    "url": "/webshop/static/js/22.d8cf5b46.chunk.js"
+  },
+  {
+    "revision": "5745a0008a15ff6d4cde",
+    "url": "/webshop/static/js/23.688fa43e.chunk.js"
+  },
+  {
+    "revision": "83d1fd818c0e0b9317bf",
+    "url": "/webshop/static/js/24.a2d5c444.chunk.js"
+  },
+  {
+    "revision": "a66aff12f188ee181e53",
+    "url": "/webshop/static/js/25.3b074ace.chunk.js"
+  },
+  {
+    "revision": "42f434874d58d4323d82",
+    "url": "/webshop/static/js/26.dc79cb7e.chunk.js"
+  },
+  {
+    "revision": "af6dc9e9c891552c9ede",
+    "url": "/webshop/static/js/27.a03399fb.chunk.js"
+  },
+  {
+    "revision": "cd35f4c3eba467c32b22",
+    "url": "/webshop/static/js/28.c5530315.chunk.js"
+  },
+  {
+    "revision": "1dbfa4bbca3359cfe863",
+    "url": "/webshop/static/js/3.79fbd8e0.chunk.js"
+  },
+  {
+    "revision": "fd4c58a91c194f7f547a",
+    "url": "/webshop/static/js/6.bf308c8d.chunk.js"
+  },
+  {
+    "revision": "0f1b34174d1111321e9c6ed75041ff11",
+    "url": "/webshop/static/js/6.bf308c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10bcc873e92b17042026",
+    "url": "/webshop/static/js/7.557c688c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/webshop/static/js/7.557c688c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "291728fd8b79cabaed32",
+    "url": "/webshop/static/js/8.d7ea3bc0.chunk.js"
+  },
+  {
+    "revision": "0041260305765c7641b5",
+    "url": "/webshop/static/js/9.ce5b2e93.chunk.js"
+  },
+  {
+    "revision": "2efe9e1838d791a422c4e3c0501cb9cf",
+    "url": "/webshop/static/js/9.ce5b2e93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "172ae1234abee80fa119",
+    "url": "/webshop/static/js/main.75abbb7f.chunk.js"
+  },
+  {
+    "revision": "dfb2090d1213f720c3d7",
+    "url": "/webshop/static/js/runtime-main.58d055c0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/webshop/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/webshop/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/webshop/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/webshop/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/webshop/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
